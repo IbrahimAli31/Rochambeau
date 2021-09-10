0a6b6ee1334db2c314fe0c2d045e25180f09c592
IbrahimAli31 <ibrahimalikore123@gmail.com>
2021-09-10 23:25:46 +0000

    add structure for computer and player points

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const computer = document.querySelector(".computer img");
+const player = document.querySelector(".player img");
+const computerPoints = document.querySelector(".computerPoints");
+const playerPoints = document.querySelector(".playerPoints");
