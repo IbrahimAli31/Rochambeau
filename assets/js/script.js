7950f02745c68395676f7440e33dd657537a5ed8
IbrahimAli31 <ibrahimalikore123@gmail.com>
2021-09-11 08:30:13 +0000

    save changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
+
 const computer = document.querySelector(".computer img");
 const player = document.querySelector(".player img");
 const computerPoints = document.querySelector(".computerPoints");
 const playerPoints = document.querySelector(".playerPoints");
+const options = document.querySelectorAll(".options button");
+
+options.forEach((option) => {
+    option.addEventListener("click", () => {
+      computer.classList.add("shakeComputer");
+      player.classList.add("shakePlayer");
+
+      setTimeout(() => {
+
+         computer.classList.remove("shakeComputer");
+        player.classList.remove("shakePlayer");
+  
+        player.src = "./" + option.innerHTML + "Player.png";
+  
