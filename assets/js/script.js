c610c22694ec43245013e646c81b5ef34f8940ef
IbrahimAli31 <ibrahimalikore123@gmail.com>
2021-09-11 20:10:07 +0000

    add alt to images and fotmat js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,59 +9,59 @@ let computerScore = 0;
 let playerScore = 0;
 
 options.forEach((option) => {
-	option.addEventListener("click", () => {
-		const userSelection = option.innerHTML.toLowerCase();
-		const computerSelection = getComputerSelection();
-		if (userSelection !== computerSelection) {
-			updateScores(userSelection, computerSelection);
-		}
+    option.addEventListener("click", () => {
+        const userSelection = option.innerHTML.toLowerCase();
+        const computerSelection = getComputerSelection();
+        if (userSelection !== computerSelection) {
+            updateScores(userSelection, computerSelection);
+        }
 
-		computer.classList.add("shakeComputer");
-		player.classList.add("shakePlayer");
-		player.src = `./assets/images/${userSelection}-image.png`;
-		computer.src = `./assets/images/${computerSelection}-image.png`;
+        computer.classList.add("shakeComputer");
+        player.classList.add("shakePlayer");
+        player.src = `./assets/images/${userSelection}-image.png`;
+        computer.src = `./assets/images/${computerSelection}-image.png`;
 
-		setTimeout(() => {
-			computer.classList.remove("shakeComputer");
-			player.classList.remove("shakePlayer");
-		}, 1000);
-	});
+        setTimeout(() => {
+            computer.classList.remove("shakeComputer");
+            player.classList.remove("shakePlayer");
+        }, 1000);
+    });
 });
 
 const getComputerSelection = () => ['rock', 'paper', 'scissors'][Math.floor(Math.random() * (2 - 0 + 1) + 0)]
 
 // This assumes that userSelection and computerSelection are different
 const updateScores = (userSelection, computerSelection) => {
-	switch (userSelection) {
-		case 'rock':
-			if (computerSelection === 'paper') {
-				computerScore++;
-			} else {
-				playerScore++;
-			}
-			break;
-		case 'paper':
-			if (computerSelection === 'scissors') {
-				computerScore++;
-			} else {
-				playerScore++;
-			}
-			break;
-		case 'scissors':
-			if (computerSelection === 'rock') {
-				computerScore++;
-			} else {
-				playerScore++;
-			}
-			break;
-		default:
-	}
-	computerPoints.innerHTML = computerScore;
-	playerPoints.innerHTML = playerScore;
+    switch (userSelection) {
+        case 'rock':
+            if (computerSelection === 'paper') {
+                computerScore++;
+            } else {
+                playerScore++;
+            }
+            break;
+        case 'paper':
+            if (computerSelection === 'scissors') {
+                computerScore++;
+            } else {
+                playerScore++;
+            }
+            break;
+        case 'scissors':
+            if (computerSelection === 'rock') {
+                computerScore++;
+            } else {
+                playerScore++;
+            }
+            break;
+        default:
+    }
+    computerPoints.innerHTML = computerScore;
+    playerPoints.innerHTML = playerScore;
 }
 resetButton.addEventListener("click", () => {
-	computerScore = 0;
-	playerScore = 0;
-	computerPoints.innerHTML = computerScore;
-	playerPoints.innerHTML = playerScore;
+    computerScore = 0;
+    playerScore = 0;
+    computerPoints.innerHTML = computerScore;
+    playerPoints.innerHTML = playerScore;
 });
